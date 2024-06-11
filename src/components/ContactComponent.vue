e5d8986c4730e7add462cefdc7686fe06ff93aac
Simret Paulos <happysimret@gmail.com>
2024-06-11 19:37:06 -0400

    modified contact component

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,240 +1,344 @@
 <template>
-  <div class="main-contact">
-    <div class="title-container" v-scrollanimation="'enter'">
-      <h1>Contact Me</h1>
-    </div>
-
+  <v-container class="main-contact">
+    <h1 class="title-container" v-scrollanimation="'enter'">Contact Me</h1>
     <p class="subtitle" v-scrollanimation="'fade'">
       Let's collaborate and work together on your project. Please feel free to
       get in touch by using the form below. I'm eager to discuss potential
       opportunities and explore how we can achieve success together.
     </p>
-
     <div class="contact-info-cont" v-scrollanimation="'stagger'">
       <div class="picture-container">
-        <div class="contact-image">
-          <img src="/images/my.jpg" alt="Contact Image">
-        </div>
+        <v-col cols="12" md="6">
+          <v-img
+            lazy-src="/images/my.jpg"
+            max-height="400px"
+            max-width="300"
+            src="/images/my.jpg"
+            alt="Contact Image"
+            class="contact-image"
+          ></v-img>
+        </v-col>
       </div>
       <div class="form-container">
         <div class="form-div">
-          <form class="v-form" @submit.prevent="submitForm">
-            <div class="form-group">
-              <label for="user_name">Name</label>
-              <input
-                id="user_name"
-                v-model="formData.user_name"
-                type="text"
-                placeholder="Enter your name"
-                required
-              >
-            </div>
-            <div class="form-group">
-              <label for="user_email_address">Email</label>
-              <input
-                id="user_email_address"
-                v-model="formData.user_email_address"
-                type="email"
-                placeholder="Enter your email"
-                required
-              >
-            </div>
-            <div class="form-group">
-              <label for="user_phone_number">Phone Number</label>
-              <input
-                id="user_phone_number"
-                v-model="formData.user_phone_number"
-                type="tel"
-                placeholder="Enter your phone number"
-                required
-              >
-            </div>
-            <div class="form-group">
-              <label for="user_message">Message</label>
-              <textarea
-                id="user_message"
-                v-model="formData.user_message"
-                placeholder="Enter your message"
-                required
-              ></textarea>
-            </div>
-            <div class="btn-container">
-              <button type="button" @click="resetForm">Clear</button>
-              <button type="submit" :disabled="!valid">Send</button>
-            </div>
-          </form>
+          <v-form v-model="valid" ref="form" class="v-form">
+            <v-row class="cont-row">
+              <v-col cols="12" md="6">
+                <v-text-field
+                  ref="user_name"
+                  class="text-custom-class"
+                  v-model="user_name"
+                  :rules="[() => !!user_name || 'This field is required']"
+                  :error-messages="errorMessages"
+                  label="Name"
+                  placeholder="name"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="6">
+                <v-text-field
+                  v-model="user_email_address"
+                  ref="user_email_address"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="12">
+                <v-text-field
+                  class="text-custom-class"
+                  v-model="user_phone_number"
+                  ref="user_phone_number"
+                  :rules="phoneNumberRules"
+                  label="Phone Number"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col class="message-cont" cols="12" md="12">
+                <v-textarea
+                  ref="user_message"
+                  v-model="user_message"
+                  :rules="[() => !!user_message || 'This field is required']"
+                  :error-messages="errorMessages"
+                  label="Feedback Message"
+                  outlined
+                  dense
+                  style="color: #f35929"
+                ></v-textarea>
+              </v-col>
+              <v-divider class="mt-12"></v-divider>
+              <v-card-actions cols="12">
+                <v-btn text @click="resetForm"> Clear </v-btn>
+                <v-spacer></v-spacer>
+                <v-slide-x-reverse-transition>
+                  <v-tooltip v-if="formHasErrors" left>
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-btn
+                        icon
+                        class="my-0"
+                        v-bind="attrs"
+                        @click="resetForm"
+                        v-on="on"
+                      >
+                        <v-icon>mdi-refresh</v-icon>
+                      </v-btn>
+                    </template>
+                    <span>Refresh form</span>
+                  </v-tooltip>
+                </v-slide-x-reverse-transition>
+                <v-btn
+                  color="primary"
+                  text
+                  :disabled="!valid"
+                  outlined
+                  @click="submitForm"
+                >
+                  Send
+                </v-btn>
+              </v-card-actions>
+            </v-row>
+          </v-form>
         </div>
       </div>
     </div>
-  </div>
+  </v-container>
 </template>
-
 <script>
 import axios from "axios";
-
 export default {
   data() {
     return {
-      formData: {
-        user_name: "",
-        user_email_address: "",
-        user_phone_number: "",
-        user_message: "",
+      user_name: null,
+      user_email_address: "",
+      user_phone_number: "",
+      user_message: "",
+      formHasErrors: false,
+      errorMessages: "",
+      rules: {
+        required: (file) => {
+          if (!file) {
+            return "'This field is required'";
+          }
+          return true;
+        },
       },
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+/.test(v) || "E-mail must be valid",
+      ],
+      phoneNumberRules: [
+        (v) => !!v || "Phone number is required",
+        (v) => /^\d{10}$/.test(v) || "Invalid phone number format",
+      ],
     };
   },
+  watch: {
+    form() {
+      this.errorMessages = "";
+    },
+  },
   computed: {
-    valid() {
-      return Object.values(this.formData).every(value => value.trim() !== '');
-    }
+    form() {
+      return {
+        user_name: this.user_name,
+        user_email_address: this.user_email_address,
+        user_phone_number: this.user_phone_number,
+        user_message: this.user_message,
+      };
+    },
   },
   methods: {
     resetForm() {
-      this.formData = {
-        user_name: "",
-        user_email_address: "",
-        user_phone_number: "",
-        user_message: "",
-      };
+      this.errorMessages = [];
+      this.formHasErrors = false;
+      Object.keys(this.form).forEach((f) => {
+        this.$refs[f].reset();
+      });
     },
     async submitForm() {
-      try {
-        await axios.post(`${process.env["VUE_APP_BASE_URL"]}/api/user_contact`, this.formData);
-        this.resetForm();
-        this.$toast.success("Thanks for getting in touch! I'll get back to you soon.", {
-          position: "top-right",
-          timeout: 4000,
-        });
-      } catch (error) {
-        this.$toast.error("Sorry, an error occurred while attempting to send your message. Please try again.", {
-          position: "top-right",
-          timeout: 4000,
-        });
+      this.formHasErrors = false;
+      Object.keys(this.form).forEach((f) => {
+        if (!this.form[f]) this.formHasErrors = true;
+        this.$refs[f].validate(true);
+      });
+      if (this.formHasErrors === false) {
+        axios
+          .request({
+            url: `${process.env["VUE_APP_BASE_URL"]}/api/user_contact`,
+            method: "POST",
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            // The data is simply the form we created above
+            data: this.form,
+          })
+          .then( ( res ) =>
+          {
+            res
+            this.resetForm();
+            this.$toast.success(
+              "Thanks for getting in touch! I'll get back to you soon."
+            );
+          })
+          .catch( ( error ) =>
+          {
+            error
+            this.$toast.error(
+              "Sorry, an error occurred while attempting to send your message. Please try again. If the issue persists, feel free to contact me for assistance.",
+              "Error"
+            );
+          });
       }
-    }
-  }
+    },
+  },
 };
 </script>
-
 <style scoped>
 .main-contact {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
+  display: grid;
+  place-items: center;
+  width: 90%;
+  color: rgb(154, 152, 152);
+  margin-top: 70px;
 }
-
-.title-container h1 {
-  color: #1bae9c;
-  font-size: 2rem;
-}
-
-.subtitle {
-  text-align: center;
-  font-size: 1rem;
-  margin-top: 20px;
-  max-width: 80%;
-  color:#a2a0a0 ;
-}
-
 .contact-info-cont {
   display: flex;
-  flex-wrap: wrap;
   width: 100%;
-  justify-content: center;
-  margin-top: 20px;
 }
-
 .picture-container {
-  width: 100%;
-  max-width: 300px;
-  background-color: #3ebaab;
   display: flex;
   justify-content: center;
-  margin: 10px;
-}
-
-.contact-image {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 80%;
-  height: auto;
-}
-
-.contact-image img {
-  max-width: 100%;
-  height: auto;
-}
-
-.form-container {
+  align-content: center;
+  padding-top: 60px;
   width: 100%;
-  max-width: 500px;
-  padding: 20px;
-  border: 2px solid #1bae9c;
-  border-radius: 8px;
-  margin: 10px;
+  max-width: 50%;
+  background-color: #1bae9c;
 }
-
-.form-div {
-  display: flex;
-  flex-direction: column;
+.title-container {
+  color: #1bae9c;
+  opacity: 0;
 }
-
-.form-group {
+.before-enter {
+  opacity: 0;
+  transform: translateY(10px);
+  transition: all 1s ease-out;
+}
+.enter {
+  opacity: 1;
+  transform: translateY(0px);
+  transition: all 1s ease-in;
+}
+.title-container,
+.subtitle {
+  text-align: center;
   margin-bottom: 20px;
 }
-
-label {
-  margin-bottom: 5px;
-  color:#1bae9c;
-  font-weight:400;
+.subtitle {
+  font-size: 1.1rem;
 }
-
-input,
-textarea {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-sizing: border-box;
-  font-size: .81rem;
-  color:#a2a0a0 ;
-  font-weight: 400;
-}
-
-.btn-container {
+.form-container {
   display: flex;
-  justify-content: flex-end;
-  margin-top: 20px;
+  align-items: center;
+  width: 100%;
+  max-width: 50%;
+  margin: 0 auto;
 }
-
-button {
-  padding: 10px 20px;
-  margin-left: 10px;
+.form-div {
+  width: 100%;
+  border: 2px solid #1bae9c;
+  border-left: none;
+  padding: 20px;
+  border-radius: 0 8px 8px 0;
+  display: grid;
+  place-items: center;
+}
+.v-form {
+  display: grid;
+  place-items: center;
+}
+.name-field,
+.email-field,
+.message-cont {
+  margin-bottom: 20px;
+}
+.cont-row {
+  width: 90%;
+}
+.btn-div {
+  display: grid;
+  place-items: end;
+}
+.btn {
   background-color: #1bae9c;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
 }
-
-button:disabled {
-  background-color: #ccc;
-  cursor: not-allowed;
+.btn :active {
+  background-color: lightgreen;
 }
-
-@media screen and (min-width: 768px) and (max-width: 1024px) {
-  .main-contact {
-    width: 70%;
+.btn:hover {
+  background-color: white;
+}
+.before-fade {
+  opacity: 0;
+  transform: translateY(10px);
+  transition: all 1s ease-out;
+}
+.fade {
+  opacity: 1;
+  transform: translateY(0px);
+  transition: all 1s ease-in;
+}
+.before-stagger {
+  opacity: 0;
+  transform: translateY(100px);
+  transition: all 1s ease-out;
+}
+.stagger {
+  opacity: 1;
+  transform: translateY(0px);
+}
+@media only screen and (max-width: 767px) {
+  .main {
+    margin-top: 20px;
   }
-  
-  .subtitle {
-    max-width: 70%;
+  .contact-info-cont {
+    flex-direction: column;
   }
-  
+  .picture-container,
+  .form-container {
+    max-width: 100%;
+  }
+  .form-div {
+    width: 100%;
+    border-radius: 8px;
+    border-radius: 8px 8px 8px 8px;
+    border: 2px solid #1bae9c;
+  }
+  .contact-info-cont {
+    flex-direction: column;
+  }
+  .picture-container,
+  .form-container {
+    max-width: 100%;
+    margin: 10px 0;
+  }
   .picture-container {
-    max-width: 250px;
+    order: 0;
+    height: 500px;
+    display:grid;
+    place-items: center;
+  }
+  .form-container {
+    order: 1;
+  }
+  .form-div {
+    width: 100%;
+    border-radius: 8px;
+  }
+  .col-md-6 .col-12{
+    padding: 0;
   }
 }
 </style>
