987c86de518bb720a297d951e43fb6cb80c10c5c
Simret Paulos <happysimret@gmail.com>
2024-06-26 16:25:44 -0400

    Updated  metadata for social media sharing on AboutPage.vue

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -13,24 +13,28 @@ export default {
     AboutComponent,
   },
   metaInfo() {
-    let baseUrl = "https://portfolio.simret.codes"; 
+    let baseUrl = "https://portfolio.simret.codes";
 
     return {
       title: "About Me - Simret's Portfolio",
       meta: [
+        // Facebook Open Graph
         { property: "og:title", content: "About Me - Simret's Portfolio" },
-        { name: "description", content: "Learn more about Simret and her achievements." },
         { property: "og:description", content: "Learn more about Simret and her achievements." },
         { property: "og:type", content: "website" },
-        { property: "og:url", content: baseUrl }, // Website URL
-        { property: "og:image", content: `${baseUrl}/public/images/website_image.png` }, 
+        { property: "og:url", content: baseUrl },
+        { property: "og:image", content: `${baseUrl}/images/website_image.png` },
+
+        // Twitter
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:title", content: "About Me - Simret's Portfolio" },
         { name: "twitter:description", content: "Learn more about Simret and her achievements." },
-        { name: "twitter:image", content: `${ baseUrl }/public/images/website_image.png` },
-        { name: "whatsapp", content: "Share on WhatsApp message" },
+        { name: "twitter:image", content: `${baseUrl}/images/website_image.png` },
+
+        // Google
         { itemprop: "name", content: "About Me - Simret's Portfolio" },
         { itemprop: "description", content: "Learn more about Simret and her achievements." },
+        { itemprop: "image", content: `${baseUrl}/images/website_image.png` },
       ],
     };
   },
